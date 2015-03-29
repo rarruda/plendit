788688c6d4196cdab859afd7b20dbe0394814b7f
R. Arruda <rarruda@plendit.com>
2015-03-29 16:35:47 +0200

    Profiles: add relations to Bookings

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,12 @@
 class Profile < ActiveRecord::Base
   has_many :ads, dependent: :destroy
+  has_many :ad_items, :through => :ad
+  has_many :bookings, :through => :ad_items
+
   #has_many :received_feedbacks, :through => :ad, foreign_key: 'profile_id', :class_name => "Feedback"
   has_many :received_feedbacks, :through => :ad, :class_name => "Feedback"
   has_many :sent_feedbacks, foreign_key: 'from_profile_id', :class_name => "Feedback"
+
   has_one :profile_status
 
   has_many :received_messages, foreign_key: 'to_profile_id', :class_name => "Messages"
