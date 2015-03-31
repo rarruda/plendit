233edfd9a3ea4c7b74981c5a72937a822e0525c8
R. Arruda <rarruda@plendit.com>
2015-04-01 00:47:28 +0200

    Improve booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,10 @@
 class Booking < ActiveRecord::Base
   belongs_to :ad_item
   belongs_to :from_profile, :class_name => "Profile"
+
   has_one :ad, through: :ad_item
   has_one :profile, through: :ad
   has_many :messages
 
-  has_one :booking_status
+  belongs_to :booking_status
 end
