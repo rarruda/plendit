1cbf8ac8b1935ea017d8d683a9e05668b9cf9693
R. Arruda <rarruda@plendit.com>
2015-04-01 00:25:44 +0200

    Improve profile model.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,11 @@
 class Profile < ActiveRecord::Base
   has_many :ads, dependent: :destroy
   has_many :ad_items, :through => :ads
-  has_many :bookings, :through => :ad_items
 
-  #has_many :received_feedbacks, :through => :ad, foreign_key: 'profile_id', :class_name => "Feedback"
+  # received_bookings == bookings:
+  has_many :bookings, :through => :ad_items
+  has_many :sent_bookings, foreign_key: 'from_profile_id', :class_name => "Booking"
+
   has_many :received_feedbacks, :through => :ads, :class_name => "Feedback"
   has_many :sent_feedbacks, foreign_key: 'from_profile_id', :class_name => "Feedback"
 
@@ -11,5 +13,4 @@ class Profile < ActiveRecord::Base
 
   has_many :received_messages, foreign_key: 'to_profile_id', :class_name => "Message"
   has_many :sent_messages, foreign_key: 'from_profile_id', :class_name => "Message"
-
 end
