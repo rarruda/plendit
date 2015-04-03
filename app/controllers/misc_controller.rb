b2fa692805d04ba950c16dc9bf20762528df3655
Rune Halvorsen <runefh@gmail.com>
2015-04-03 15:08:36 +0200

    Cleaned up top-bar partial and code

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,6 +1,8 @@
 class MiscController < ApplicationController
   def frontpage
     @show_search_field = true
+    # Fixme: this should just be the current logged in user object, or nil, not a bool
+    @has_user = true
   end
 
   def ad
