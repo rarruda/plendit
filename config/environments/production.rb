4b573638b13b009f5493f1a614aa1a33c1b315a9
R. Arruda <rarruda@plendit.com>
2015-04-19 21:49:57 +0200

    Disable email sending from devise in prod. just to get app to play nice in heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  # ^^^^ -- its ok if we are not able to send emails.
 
   # Devise needs this to be set. For now we say that heroku (dev.plendit.com) is the url for prod.
   config.action_mailer.default_url_options = { :host => 'dev.plendit.com' }
