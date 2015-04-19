e148bc63aec50f9c6b7addf2c88300c8055235ea
R. Arruda <rarruda@plendit.com>
2015-04-19 15:12:13 +0200

    Validate feedback body being within certain limits

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,6 @@ class Feedback < ActiveRecord::Base
   belongs_to :ad
   belongs_to :from_user, :class_name => "User"
   has_one :user, :through => :ad
+
+  validates :body, presence: true, length: { in: 5..2048 }
 end
