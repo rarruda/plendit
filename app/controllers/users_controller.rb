5c40135945be37ce35d23e367f37b0aac6c3fd85
R. Arruda <rarruda@plendit.com>
2015-04-26 14:44:23 +0200

    User: small improvements in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :confirmation]
   #before_filter :authenticate_user!, :except => [:finish_signup]
 
-  # GET /users
-  # GET /users.json
   def index
-    @users = User.all
   end
 
   # GET /users/1
