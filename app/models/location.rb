b79213a8de94eff78ed6553e2b925777fd686349
R. Arruda <rarruda@plendit.com>
2015-04-27 23:25:32 +0200

    Add geocoding when updating a location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ActiveRecord::Base
   belongs_to :user
+  geocoded_by :address, :latitude => :lat, :longitude => :lon
+  after_validation :geocode, if: ->(obj){ obj.address_line.present? and obj.changed? }
+
+  def address
+    [address_line, city, state, "Norway"].compact.join(', ')
+  end
 end
