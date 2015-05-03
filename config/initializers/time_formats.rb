547f34e6d2ae2ed5141f4cb25d7264c23d748ca2
Rune Halvorsen <runefh@gmail.com>
2015-05-03 13:27:17 +0200

    Added format for datetime

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,4 @@
 # fixme: should be zero padded, and day should be lowercase.
 #        Maybe there's a localiztion framework for that?
 Time::DATE_FORMATS[:plendit_short_date] = '%d. %b %Y'
+Time::DATE_FORMATS[:plendit_short_datetime] = '%d. %b %Y, %H.%M'
