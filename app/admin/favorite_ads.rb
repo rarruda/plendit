30ed87a0529846cfd417ffb60d504822cd83bd51
R. Arruda <rarruda@plendit.com>
2015-05-16 16:06:26 +0200

    Favorites: add admin management

---
+++ app/admin/favorite_ads.rb
@@ -0,0 +1,33 @@
+ActiveAdmin.register FavoriteAd do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  menu :priority => 6
+  #actions :index, :show
+
+  permit_params :ad_id
+
+  index do
+    selectable_column
+    id_column
+    column :favorite_list
+    column :ad
+
+    column :created_at
+    actions
+  end
+
+
+end
---
+++ app/admin/favorite_lists.rb
@@ -0,0 +1,33 @@
+ActiveAdmin.register FavoriteList do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  menu :priority => 6
+  #actions :index, :show
+
+  permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :name
+
+    column :created_at
+    actions
+  end
+
+
+end
