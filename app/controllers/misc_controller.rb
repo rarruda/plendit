f9be5a2022d6820ca3d771bddd8038166dbe3fb6
Rune Halvorsen <runefh@gmail.com>
2015-05-24 12:59:46 +0200

    Removed unused temp controllers for misc

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -4,15 +4,4 @@ class MiscController < ApplicationController
     @hide_search_field = true
   end
 
-  def result
-    @ads = Ad.where('LOWER(title) LIKE LOWER(?) OR LOWER(body) LIKE LOWER(?)', "%#{params[:q]}%", "%#{params[:q]}%" )
-    @term = params[:q]
-  end
-
-  # work in progress page for experimenting with partials and stuff. Don't refactor yet
-  def wip
-    @ads = Ad.where('LOWER(title) LIKE LOWER(?) OR LOWER(body) LIKE LOWER(?)', "%#{params[:q]}%", "%#{params[:q]}%" )
-    @ad = @ads[0]
-  end
-
 end
