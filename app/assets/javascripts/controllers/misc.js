da57cdf27f95878f6120fc673030f2093d6c103f
Rune Halvorsen <runeh@plendit.com>
2015-06-07 11:17:55 +0200

    Moved JS controllers into separate files, rather than main

---
+++ app/assets/javascripts/controllers/misc.js
@@ -0,0 +1,56 @@
+window.controllers = window.controllers || {};
+
+window.controllers.tagCreator = {
+    callable: function(ele) {
+        var inEle = ele;
+        var outEle = document.querySelector("[data-payout]");
+
+        inEle.addEventListener('change', syncPayout);
+        inEle.addEventListener('keyup', syncPayout);
+        syncPayout();
+
+        function syncPayout() {
+            var price = parseFloat(inEle.value);
+            console.log(price)
+            if (isNaN(price)) {
+                outEle.value = "";
+            }
+            else {
+                if (price == 0) {
+                    outEle.value = 0;
+                }
+                else {
+                    outEle.value = price * 0.90;
+                }
+            }
+        }
+    }
+};
+
+window.controllers.resultContainerSizeAdjuster = {
+    dependencies: ["$element", "eventbus"],
+    callable: function(ele, eventBus) {
+        window.addEventListener('resize', adjustHeight);
+        eventBus.on('filter-toggle', adjustHeight);
+        adjustHeight();
+
+        function adjustHeight() {
+            var height = window.innerHeight - ele.offsetTop;
+            ele.style.height = height + "px";
+        }
+    }
+}
+
+window.controllers.togglableFilters = {
+    dependencies: ["$element", "eventbus"] ,
+    callable: function(ele, eventBus) {
+        var toggleEle = ele.querySelector("[data-filter-toggler]");
+        var filterEle = ele.querySelector("[data-filter-container]");
+        toggleEle.addEventListener('click', toggle);
+
+        function toggle() {
+            filterEle.classList.toggle("u-hidden");
+            eventBus.emit('filter-toggle');
+        }
+    }
+}
---
+++ app/assets/javascripts/controllers/syncpayout.js
@@ -0,0 +1,40 @@
+window.controllers = window.controllers || {};
+
+window.controllers.syncPayout = {
+    callable: function(root) {
+        init();
+
+        function init() {
+            var inputs = root.querySelectorAll("[data-payout-source]");
+            for (var n = 0, e; e = inputs[n++];) {
+                initInput(e);
+            }
+        }
+
+        function initInput(source) {
+            var destination = root.querySelector("[data-payout-for=\"" + source.getAttribute("name") + "\"]");
+            var syncer = makeSyncer(source, destination);
+            source.addEventListener('change', syncer);
+            source.addEventListener('keyup', syncer);
+            syncer();
+        }
+
+        function makeSyncer(source, destination) {
+            return function(evt) {
+                var price = parseFloat(source.value);
+                if (isNaN(price)) {
+                    destination.value = "";
+                }
+                else {
+                    if (price == 0) {
+                        destination.value = 0;
+                    }
+                    else {
+                        destination.value = price * 0.90;
+                    }
+                }
+
+            }
+        }
+    }
+};
