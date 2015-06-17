47a12579294f26aa8348ddf09013f5f240e06357
Rune Halvorsen <runefh@gmail.com>
2015-06-18 01:44:22 +0200

    Moved frontpage popular topics thingy into yaml config

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,54 +1,11 @@
 class MiscController < ApplicationController
 
   @@postal_codes = YAML.load_file("#{Rails.root}/config/data/postal_codes.yml")
+  @@popular_topics = YAML.load_file("#{Rails.root}/config/data/popular_topics.yml")
 
   def frontpage
     @hide_search_field = true
-
-    @popular_topics = [
-      {
-        title: "Verktøy",
-        summary: "Finn verktøy for alle oppgaver rett i nærheten av der du bor.",
-        items: [
-          {
-            title: "Drill",
-            link: "link1",
-            image: "promo_imgs/drill.jpg"
-          },
-          {
-            title: "Flisekutter",
-            link: "link2",
-            image: "promo_imgs/flisekutter.jpg"
-          },
-          {
-            title: "Stige",
-            link: "link3",
-            image: "promo_imgs/stige.jpg"
-          }
-        ]
-      },
-      {
-        title: "Friluft",
-        summary: "Finn telt eller annet friluftsutstyr.",
-        items: [
-          {
-            title: "Lavvo",
-            link: "link1",
-            image: "promo_imgs/lavvo.jpg"
-          },
-          {
-            title: "Pulk",
-            link: "link2",
-            image: "promo_imgs/pulk.jpg"
-          },
-          {
-            title: "Telt",
-            link: "link3",
-            image: "promo_imgs/telt.jpg"
-          }
-        ]    
-      }
-    ]
+    @popular_topics = @@popular_topics
   end
 
   def postal_place
