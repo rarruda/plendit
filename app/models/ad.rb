18277059bd0c1f3d42a18b2ee5ce2e03a215a0b5
Rune Halvorsen <runefh@gmail.com>
2015-07-05 21:22:11 +0200

    Fixed paths to placeholders

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -138,10 +138,10 @@ class Ad < ActiveRecord::Base
   ## default fallback to stock images. (so that it will degrate nicely)
   def image_url_with_fallback( size )
     stock_images = {
-      thumb: 'no_image_180x120.jpg',
-      searchresult: 'no_image_450x300.jpg',
-      hero: 'no_image_900x600.jpg',
-      gallery: 'no_image_900x600.jpg'
+      thumb: ActionController::Base.helpers.image_path('no_image_180x120.jpg'),
+      searchresult: ActionController::Base.helpers.image_path('no_image_450x300.jpg'),
+      hero: ActionController::Base.helpers.image_path('no_image_900x600.jpg'),
+      gallery: ActionController::Base.helpers.image_path('no_image_900x600.jpg')
     }
 
     if not stock_images.keys.include? size
