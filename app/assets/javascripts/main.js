3a308fba020baf256dbe2bd3f137b16da4168b1a
Rune Halvorsen <runeh@plendit.com>
2015-07-14 18:38:36 +0200

    Use event constants on the eventbus

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
+
+    var eventbus = smokesignals.convert({
+        IMAGES_CHANGED: 'images_changed_event'
+    });
+
     var c = new Controllerator();
-    c.registerInstance('eventbus', smokesignals.convert({}));
+    c.registerInstance('eventbus', eventbus);
     c.registerInstance('tagbox', tagBox);
     c.registerInstance('xhr', xhr);
     c.scanControllers(controllers);
