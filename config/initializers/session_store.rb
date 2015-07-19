a5a829610372b0cbcc29e5847ca7a28363567c9d
R. Arruda <rarruda@plendit.com>
2015-07-19 14:52:02 +0200

    Add reminder for when deploying in prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
+## NOTE: :secure => true should be configured to allow only logins over HTTPS
+# if ENV != 'production'
 Rails.application.config.session_store :cookie_store, key: '_plendit_session'
+# else
+#   Rails.application.config.session_store :cookie_store, { key: '_plendit_session', :secure => true }
+# end
