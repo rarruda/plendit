a7cc4a71815d35cc84e112d0300d66488ef46579
R. Arruda <rarruda@plendit.com>
2015-07-21 21:04:52 +0200

    Result of migration is a new database schema baseline.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720191954) do
+ActiveRecord::Schema.define(version: 20150721171737) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -50,24 +50,6 @@ ActiveRecord::Schema.define(version: 20150720191954) do
   end
 
   add_index "ad_items", ["ad_id"], name: "index_ad_items_on_ad_id"
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
   create_table "ads", force: :cascade do |t|
     t.integer  "user_id"
