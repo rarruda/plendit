f0bc669ea2b917c845c9603176438fee6597c5eb
R. Arruda <rarruda@plendit.com>
2015-07-23 01:20:30 +0200

    Stub for when we start to seriously start doing i10n and l18n

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Plendit
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    ### For enabling translations in subdirectories:
+    ###config.i18n.load_path += Dir[File.join(Rails.root, 'config', 'locales', '**', '*.{rb,yml}')]
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/lib)
