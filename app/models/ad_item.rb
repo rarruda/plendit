30205e664bc963757442e310b00de18972ea4d3e
R. Arruda <rarruda@plendit.com>
2015-07-24 02:15:54 +0200

    Calendars now show actual data of confirmed bookings.

--- app/models/ad_item.rb
+++ app/models/ad_item.rb
@@ -1,4 +1,38 @@
 class AdItem < ActiveRecord::Base
   belongs_to :ad
   has_many :bookings
+
+
+ def availability_for_month( year = Date.today.year , month = Date.today.month )
+    #AVAILABILITY_STATUSES = ['booked', 'starting', 'ending', 'available'] #'past', 'today'
+
+    ##correct:
+    bookings = Booking.active.ad_item( self.id ).exclude_past.in_month(year,month).order( :starts_at )
+    # needs to have 'active', otherwise we show unconfirmed bookings as fully booked.
+    # FOR debugging:
+    #bookings = Booking.ad_item( self.id ).exclude_past.in_month(year,month).order( :starts_at )
+    # logger.debug pp bookings
+
+    availability_cal = {}
+
+    ( Date.new(year,month).beginning_of_month ... Date.new(year,month).end_of_month ).each do |d|
+      if d.past?
+        availability_cal[d.day] = 'past'
+      elsif d == Date.today
+        # FIXME: Today is totally broken. we probably want to have that in adition of whatever other status we have:
+        availability_cal[d.day] = 'today'
+      else
+        availability_cal[d.day] = 'available'
+        # TODO: Optimize. no need to loop over all bookings on every date.
+        bookings.each { |b|
+          if availability_cal[d.day] == 'available'
+            availability_cal[d.day] = b.date_status(d)
+          end
+        }
+      end
+    end
+
+    availability_cal
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ActiveRecord::Base
+  include Comparable
+
   extend TimeSplitter::Accessors
 
   split_accessor :starts_at
@@ -23,6 +25,9 @@ class Booking < ActiveRecord::Base
   scope :in_month,   ->(year,month) { where( 'ends_at >= ? and starts_at <= ?',
     DateTime.new(year, month).beginning_of_month, DateTime.new(year, month).end_of_month ) }
 
+  scope :exclude_past,   ->{ where( 'ends_at >= ?', DateTime.now ) }
+
+
   validates :starts_at, :ends_at, :overlap => {
     :scope         => "ad_item_id",
     :query_options => { :active => nil }
@@ -44,5 +49,61 @@ class Booking < ActiveRecord::Base
   def duration_in_days
     (self.ends_at.to_date - starts_at.to_date).to_i
   end
+
+  ###
+  ###
+  def include?(date)
+    ( date >= self.starts_at and date <= self.ends_at )
+  end
+
+
+  def date_status(date)
+    #AVAILABILITY_STATUSES = ['booked', 'starting', 'ending', 'available']
+
+    # I end before date.beginning_of_day
+    if self.ends_at < date.to_datetime.beginning_of_day
+      'available'
+    # I start after date.end_of_day
+    elsif self.starts_at > date.to_datetime.end_of_day
+      'available'
+    else
+      # I start after date.beginning_of_day _and_ before date.end_of_day
+      if self.starts_at > date.to_datetime.beginning_of_day and
+        self.starts_at <= date.to_datetime.end_of_day
+        'starting'
+      # I end after date.beginning_of_day _and_ before date.end_of_day
+      elsif self.ends_at > date.to_datetime.beginning_of_day and
+        self.ends_at < date.to_datetime.end_of_day
+        'ending'
+      else
+        ### hornets nest of possible partially available in that day/corner cases....
+        #### (if something is rented for some hours within the day, the day will be marked as fully 'booked')
+        'booked'
+      end
+    end
+  end
+
+  # Comparable
+  def <=>(other)
+    if self.starts_at < other.starts_at
+      -1
+    elsif self.starts_at == other.starts_at and self.ends_at == other.ends_at
+      0
+    else
+      1
+    end
+  end
+
+  # copare it with a date?
+  # TODO: consider merging with the comparable between bookings above.
+  #def <=>(date)
+  #  if self.ends_at.date < date
+  #    -1
+  #  elsif date >= self.starts_at.date and date <= self.ends_at.date
+  #    0
+  #  else
+  #    1
+  #  end
+  #end
 
 end
