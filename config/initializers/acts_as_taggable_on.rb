e4007ee447630f77017fce5c8a6067d4e4d8f264
R. Arruda <rarruda@plendit.com>
2015-08-15 18:25:49 +0200

    Add config for Acts_As_Taggable_On even if we just stick to the defaults

---
+++ config/initializers/acts_as_taggable_on.rb
@@ -0,0 +1,17 @@
+# If you would like to remove unused tag objects after removing taggings, add:
+#ActsAsTaggableOn.remove_unused_tags = true
+
+# If you want force tags to be saved downcased:
+#ActsAsTaggableOn.force_lowercase = true
+
+# If you want tags to be saved parametrized (you can redefine to_param as well):
+#ActsAsTaggableOn.force_parameterize = true
+
+# If you would like tags to be case-sensitive and not use LIKE queries for creation:
+#ActsAsTaggableOn.strict_case_match = true
+
+# If you would like to have an exact match covering special characters with MySql:
+#ActsAsTaggableOn.force_binary_collation = true
+
+# If you want to change the default delimiter (it defaults to ','). You can also pass in an array of delimiters such as ([',', '|']):
+#ActsAsTaggableOn.delimiter = ','
