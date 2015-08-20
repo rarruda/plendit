f66c4676283162674926c8da6a0072e1aae9c2fe
R. Arruda <rarruda@plendit.com>
2015-08-20 18:37:36 +0200

    Move popular topics to global config

--- config/initializers/custom_application_config.rb
+++ config/initializers/custom_application_config.rb
@@ -3,7 +3,8 @@
 #Plendit::Application.config.x.payment_processing.schedule = :daily
 #Plendit::Application.config.x.payment_processing.retries  = 3
 #Plendit::Application.config.x.super_debugger = true
-
+# read as:
+#Rails.configuration.x.super_debugger
 
 include ActionView::Helpers::NumberHelper
 
@@ -16,3 +17,30 @@ Plendit::Application.config.x.map.default_zoom_level         = 6
 
 
 Plendit::Application.config.x.customerservice.email          ='NOSPAM__kundesenter@plendit.no'
+
+Plendit::Application.config.x.frontpage.popular_ads = [
+  { title: 'Bil',
+    term:  'bil',
+    image: 'promo_imgs/bil.jpg'
+  },
+  { title: 'Bolig',
+    term:  'bolig',
+    image: "promo_imgs/bolig.jpg"
+  },
+  { title: 'Verktøy',
+    term:  'verktøy',
+    image: "promo_imgs/verktoy.jpg"
+  },
+  { title: 'Scooter',
+    term:  'scooter',
+    image: "promo_imgs/scooter.jpg"
+  },
+  { title: 'Sykkel',
+    term:  'sykkel',
+    image: "promo_imgs/sykkel.jpg"
+  },
+  { title: 'Telt',
+    term:  'telt',
+    image: "promo_imgs/telt.jpg"
+  }
+]
