69fd3921bc80715828369c85826fa89356710d9b
Rune Halvorsen <runeh@plendit.com>
2015-08-20 19:17:23 +0200

    Remove reference to non-extant popular topics file

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,11 +1,10 @@
 class MiscController < ApplicationController
 
   @@postal_codes = YAML.load_file("#{Rails.root}/config/data/postal_codes.yml")
-  @@popular_topics = YAML.load_file("#{Rails.root}/config/data/popular_topics.yml")
 
   def frontpage
     @hide_search_field = true
-    @popular_topics = @@popular_topics
+    @popular_topics = Rails.configuration.x.popular_topics
   end
 
   def postal_place
