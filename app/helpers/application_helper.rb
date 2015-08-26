843c2ec8176c0f54f19c4aba20c6353429c6c8dc
R. Arruda <rarruda@plendit.com>
2015-08-26 18:49:22 +0200

    bugfix: return empty string for nil values of currency when displaying

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
   def number_to_currency_pretty( num, options = {unit: ''} )
-    number_to_currency( num, options ).gsub(/\,00 /, ",- ")
+    number_to_currency( ( num || '' ), options ).gsub(/\,00 /, ",- ")
   end
 end
