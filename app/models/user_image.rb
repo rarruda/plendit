62078eb38e331c9f0e5ba1e214a4ed9a9a5c897c
R. Arruda <rarruda@plendit.com>
2015-09-06 13:27:28 +0200

    bugfix on user_image validation

--- app/models/user_image.rb
+++ app/models/user_image.rb
@@ -4,6 +4,6 @@ class UserImage < ActiveRecord::Base
   enum type: { avatar: 0, logo: 1 }
 
   has_attached_file :image, styles: { avatar_huge: "120x120>", avatar_medium: "64x64>" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
