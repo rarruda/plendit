db60598a57f419d2a9e4c463ae27a21d2301cf85
R. Arruda <rarruda@plendit.com>
2015-09-06 12:59:01 +0200

    Bugfix on paperclip initializer config syntax

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,15 +3,13 @@
 
 Paperclip.options[:command_path] = "~/homebrew/bin/"
 
-config.paperclip_defaults = {
-  :storage => :s3,
-  :url     => ":s3_domain_url",
-  :s3_host_name => ENV['PCONF_S3_HOST_NAME'],
-  :s3_credentials => {
-    :bucket => ENV['PCONF_S3_BUCKET_NAME'],
-    :access_key_id => ENV['PCONF_AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['PCONF_AWS_SECRET_ACCESS_KEY']
-  }
+Paperclip::Attachment.default_options[:storage]      = :s3
+Paperclip::Attachment.default_options[:url]          = ":s3_domain_url"
+Paperclip::Attachment.default_options[:s3_host_name] = ENV['PCONF_S3_HOST_NAME']
+Paperclip::Attachment.default_options[:s3_credentials] = {
+  :bucket            => ENV['PCONF_S3_BUCKET_NAME'],
+  :access_key_id     => ENV['PCONF_AWS_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['PCONF_AWS_SECRET_ACCESS_KEY']
 }
 
 # Allow interpolation of :ad_id
