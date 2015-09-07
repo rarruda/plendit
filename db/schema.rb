1333a3d1ba0559e83602e9c2689b7efc0158f833
R. Arruda <rarruda@plendit.com>
2015-09-08 01:45:19 +0200

    Add DB contraints to GUIDs in bookings are always unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907213655) do
+ActiveRecord::Schema.define(version: 20150907234348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20150907213655) do
 
   add_index "bookings", ["ad_item_id"], name: "index_bookings_on_ad_item_id", using: :btree
   add_index "bookings", ["from_user_id"], name: "index_bookings_on_from_user_id", using: :btree
+  add_index "bookings", ["guid"], name: "index_bookings_on_guid", unique: true, using: :btree
   add_index "bookings", ["status"], name: "index_bookings_on_status", using: :btree
 
   create_table "favorite_ads", force: :cascade do |t|
