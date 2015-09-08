34de01a4612c2c0cfe6ab8f05038da5f9e2b9d24
R. Arruda <rarruda@plendit.com>
2015-09-08 20:38:10 +0200

    Added validations and auto-population of iban field for UserBankAccount. Included rspec tests for bonus points

--- app/models/user_bank_account.rb
+++ app/models/user_bank_account.rb
@@ -1,3 +1,40 @@
 class UserBankAccount < ActiveRecord::Base
   belongs_to :user
+
+  before_validation :normalize_account_number
+  before_validation :set_iban
+
+  validates :account_number,
+    uniqueness: true,
+    length: { is: 11 },
+    format: { with: /\A[0-9]{11}\z/,
+      message: "only numbers, 11 digits" }
+
+  validate :account_number,
+    if: :is_account_number_valid?
+
+  validates :user_id, presence: true
+  validate :iban, if: :is_iban_valid?
+
+
+  private
+  def set_iban
+    ibanizator = Ibanizator.new
+    self.iban = ibanizator.calculate_iban country_code: :no, account_number: self.account_number
+  end
+
+  # only valid for norway:
+  def is_account_number_valid?
+    # remove check_digit, calculate Mod11, and see if it still matches:
+    return false if self.account_number.length != 11
+    Mod11.new( self.account_number[0..-2] ).full_value == self.account_number
+  end
+
+  def is_iban_valid?
+    Ibanizator.iban_from_string(self.iban).valid?
+  end
+
+  def normalize_account_number
+    self.account_number.gsub!(/(\s|\.)+/, "")
+  end
 end
--- spec/models/user_bank_account_spec.rb
+++ spec/models/user_bank_account_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe UserBankAccount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should accept a valid norwegian bank account number' do
+    expect( FactoryGirl.create(:user_bank_account) ).to be_valid
+  end
+
+  it 'should not accept a bank account number with an invalid account number' do
+    expect( FactoryGirl.build(:user_bank_account, account_number: '15032080118') ).not_to be_valid
+  end
+
+  it 'should accept a norwegian bank account number with numbers and/or periods in it' do
+    expect( FactoryGirl.build(:user_bank_account, account_number: '1503 20..80119') ).to be_valid
+  end
+
+  it 'should not accept a bank account number with the wrong number of digits' do
+    expect( FactoryGirl.build(:user_bank_account, account_number: '150320801199999') ).not_to be_valid
+  end
+
+  it 'should not accept a bank account number with letters in it' do
+    expect( FactoryGirl.build(:user_bank_account, account_number: '15032080foo') ).not_to be_valid
+  end
+
+  it 'should automatically generate an iban number from a bank account number' do
+    expect( FactoryGirl.create(:user_bank_account, account_number: '15032080119').iban ).to be == "NO5015032080119"
+  end
+
+  it 'should not accept a bank account number without a user connected to it' do
+    expect( FactoryGirl.build(:user_bank_account, user_id: nil) ).not_to be_valid
+  end
 end
