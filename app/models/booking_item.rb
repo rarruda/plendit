8bbfb8f848c4fff4e6bcb6d49925e61415a7ab4c
R. Arruda <rarruda@plendit.com>
2015-09-08 16:40:29 +0200

    Better describe BookingItem categories

--- app/models/booking_item.rb
+++ app/models/booking_item.rb
@@ -1,5 +1,5 @@
 class BookingItem < ActiveRecord::Base
   belongs_to :booking
 
-  enum category: { reserved_for_ad: 1, insurance: 2, platform_fee: 3, vat: 5 }
+  enum category: { reserved_for_ad_item: 1, insurance_fee: 2, insurance_deposit: 3, platform_fee: 4, vat: 5 }
 end
