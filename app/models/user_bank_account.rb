554fc98a5f7a7a272dd0c8e02adb2d67963e9994
R. Arruda <rarruda@plendit.com>
2015-09-09 23:37:41 +0200

    Fix user_bank_account validations, so that it actually works now

--- app/models/user_bank_account.rb
+++ app/models/user_bank_account.rb
@@ -4,17 +4,10 @@ class UserBankAccount < ActiveRecord::Base
   before_validation :normalize_account_number
   before_validation :set_iban
 
-  validates :account_number,
-    uniqueness: true,
-    length: { is: 11 },
-    format: { with: /\A[0-9]{11}\z/,
-      message: "only numbers, 11 digits" }
-
-  validate :account_number,
-    if: :is_account_number_valid?
-
   validates :user_id, presence: true
-  validate :iban, if: :is_iban_valid?
+  validates :account_number, uniqueness: {message: "(account number) is in use by another user in plendit."}
+  validate  :is_account_number_valid?
+  validate  :is_iban_valid?
 
 
   private
@@ -24,13 +17,14 @@ class UserBankAccount < ActiveRecord::Base
 
   # only valid for norway:
   def is_account_number_valid?
+    # check a regex to see if its a 11 digit number
+    errors.add(:account_number, "invalid account number (only numbers, 11 digits)") if self.account_number !~ /\A[0-9]{11}\z/
     # remove check_digit, calculate Mod11, and see if it still matches:
-    return false if self.account_number.length != 11
-    Mod11.new( self.account_number[0..-2] ).full_value == self.account_number
+    errors.add(:account_number, "invalid account number (wrong checksum)") if ( Mod11.new( self.account_number[0..-2] ).full_value != self.account_number )
   end
 
   def is_iban_valid?
-    Ibanizator.iban_from_string(self.iban).valid?
+    errors.add(:iban, "generated iban account number is invalid") if Ibanizator.iban_from_string(self.iban).valid?
   end
 
   def normalize_account_number
