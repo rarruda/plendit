e8ea279e9fac6fe1ab35729dfe313165cfbb7b20
R. Arruda <rarruda@plendit.com>
2015-09-11 18:18:35 +0200

    fix link to help/ticket system

--- config/initializers/custom_application_config.rb
+++ config/initializers/custom_application_config.rb
@@ -16,8 +16,9 @@ Plendit::Application.config.x.map.default_center_coordinates = { lat: 61.5, lon:
 Plendit::Application.config.x.map.default_zoom_level         = 6
 Plendit::Application.config.x.map.google_maps_js_api_key     ='AIzaSyAwciykMKFfGsTiDrqAwg80C5FCSq6vQr8'
 
-Plendit::Application.config.x.customerservice.email          ='NOSPAM__kundesenter@plendit.no'
-Plendit::Application.config.x.customerservice.website        ='https://plendit.freshdesk.com/support/home'
+Plendit::Application.config.x.customerservice.email          = 'kundesenter__at__plendit.no'
+Plendit::Application.config.x.customerservice.contact_form   = 'https://plendit.zendesk.com/hc/no/requests/new'
+Plendit::Application.config.x.customerservice.website        = 'https://plendit.zendesk.com/'
 
 Plendit::Application.config.x.organization.phone_number      ='+47 97 04 43 99'
 Plendit::Application.config.x.organization.org_id            ='915 252 230'
