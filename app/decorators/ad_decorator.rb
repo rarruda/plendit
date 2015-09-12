1ba598774d2ec86e172481cda944feae569c31a1
R. Arruda <rarruda@plendit.com>
2015-09-12 17:34:15 +0200

    Bugfix: Nice to remember to commit the decorator

---
+++ app/decorators/ad_decorator.rb
@@ -0,0 +1,32 @@
+class AdDecorator < Draper::Decorator
+  delegate_all
+  #delegate :body, :id, :title, :user, :price, :status, :summary, :ad_images, :received_feedbacks, :published?, :is_favorite_of
+
+  def price
+    return nil if object.price.nil?
+    ( ( object.price / 100).to_i + ( object.price/100.0  ).modulo(1) )
+  end
+
+  def image_url( param )
+    object.safe_image_url( param )
+  end
+
+  def title
+    object.title.blank? ?  "(Ingen tittel)" : object.title
+  end
+
+  def to_param
+    [self.id, (object.title || '').parameterize].join('----')[0,64]
+  end
+
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+end
