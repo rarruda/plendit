947edf95bfc5360f63730ed94fd5134e739969c2
R. Arruda <rarruda@plendit.com>
2015-09-14 21:15:07 +0200

    Add reminders to fix showing correct values of a booking

--- app/decorators/booking_decorator.rb
+++ app/decorators/booking_decorator.rb
@@ -4,4 +4,6 @@ class BookingDecorator < Draper::Decorator
   decorates_association :user
   decorates_association :from_user
   decorates_association :messages
+
+  # need decorator for sum_paid_to_owner, sum_paid_by_renter
 end
