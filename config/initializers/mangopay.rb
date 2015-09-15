80b666b4f284142e051d87e7bba4ad267729f95f
R. Arruda <rarruda@plendit.com>
2015-09-16 00:47:44 +0200

    MangoPay config: do not pretend that strings are booleans

--- config/initializers/mangopay.rb
+++ config/initializers/mangopay.rb
@@ -1,5 +1,5 @@
 MangoPay.configure do |c|
-  c.preproduction     = ENV['PCONF_MANGOPAY_PREPRODUCTION']
+  c.preproduction     = ENV['PCONF_MANGOPAY_PREPRODUCTION'] == "false" ? false : true
   c.client_id         = ENV['PCONF_MANGOPAY_CLIENT_ID']
   c.client_passphrase = ENV['PCONF_MANGOPAY_CLIENT_PASSPHRASE']
 end
