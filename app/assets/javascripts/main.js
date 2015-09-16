c20d258197b6b288702fdde38fcfc183adfcfe00
Rune Halvorsen <runeh@plendit.com>
2015-09-16 21:56:16 +0200

    Added card.js Added add credit card partial skeleton

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -14,6 +14,7 @@ function main() {
     c.registerInstance('tagbox', tagBox);
     c.registerInstance('xhr', xhr);
     c.registerInstance('createElement', createElement);
+    c.registerInstance('Card', Card);
     c.registerInstance('utils', plenditUtils);
     c.scanControllers(controllers);
     c.scanServices(services);
