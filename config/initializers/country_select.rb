569d27a839de1084dae0843c9b31554bc7495ca8
R. Arruda <rarruda@plendit.com>
2015-09-25 19:00:10 +0200

    country list should be localized

--- config/initializers/country_select.rb
+++ config/initializers/country_select.rb
@@ -1,5 +1,6 @@
 CountrySelect::FORMATS[:with_flag] = lambda do |country|
-  "#{country.name}"
+  country_name = country.translations[I18n.locale.to_s] || country.name
+  "#{country_name}"
   # For when we add flags on the dropdown:
-  # (cc:#{country.alpha2}) #{flag(country.alpha2)}"
+  #"(cc:#{country.alpha2}) #{flag(country.alpha2)} country_name"
 end
