cf9fb50b50fd2cc5af83b68283e663d56bd783d8
R. Arruda <rarruda@plendit.com>
2015-09-29 18:18:20 +0200

    added mock geocoding for location, to avoid actual calls to the api

--- spec/factories/location.rb
+++ spec/factories/location.rb
@@ -1,6 +1,23 @@
 #require 'faker'
 #Faker::Config.locale = 'nb-NO'
 
+Geocoder.configure(:lookup => :test)
+
+Geocoder::Lookup::Test.add_stub(
+  "Bentsebrugata 23B, 0469, Oslo", [
+    {
+      'latitude'     => 59.9382258,
+      'longitude'    => 10.7611172,
+      'address'      => 'Bentsebrugata 23B, 0469 Oslo, Norway',
+      'city'         => 'Oslo',
+      'state'        => 'Oslo',
+      'postal_code'  => '0469',
+      'country'      => 'Norway',
+      'country_code' => 'NO'
+    }
+  ]
+)
+
 FactoryGirl.define do
   factory :location do |f|
     f.address_line  { "Bentsebrugata 23B" }
