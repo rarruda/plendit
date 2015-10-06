3c44e0fe1d25b5c99f7d52843b2eee2add3bd4ba
Rune Halvorsen <runeh@plendit.com>
2015-10-06 15:29:48 +0200

    Fixed insurance checkbox not triggering recalc

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -15,7 +15,7 @@ class MiscController < ApplicationController
     category = params[:category].to_sym
     insurance = params[:insurance] == 'true' || Plendit::Application.config.x.insurance.is_required[category]
     final_price  = price * ( 1.0 + Plendit::Application.config.x.platform.fee_in_percent )
-    final_price += price * Plendit::Application.config.x.insurance.price_in_percent[category.to_sym] if insurance == 'true'
+    final_price += price * Plendit::Application.config.x.insurance.price_in_percent[category.to_sym] if insurance
     render text: '%.2f' % (final_price / 100)
   end
 end
