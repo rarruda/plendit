71df3002ac656e3e1b5bb96dc73221e22a3ef8db
Rune Halvorsen <runeh@plendit.com>
2015-10-10 18:25:53 +0200

    Added helper for avatar image

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -4,6 +4,17 @@ module AvatarHelper
     image_tag 'plendbot/robot_profile_01.png', alt: '', class: "avatar--#{size}", size: size
   end
 
-  # todo: add a helper for regular avatar as well
+  def avatar_image(user, size, options = {})
+    sizes = {
+      huge:   '120x120',
+      large:  '80x80',
+      medium: '64x64',
+      small:  '48x48',
+      tiny:   '24x24'
+    }
+
+    css_class = "avatar avatar--#{size} #{options[:class]}"
+    image_tag user.safe_avatar_url, alt: '', class: css_class, size: sizes[size]
+  end
 
 end
