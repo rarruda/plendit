3312914b01335defd724f2a792f2fec48872d15a
Rune Halvorsen <runeh@plendit.com>
2015-10-11 18:30:37 +0200

    Helper for ad label

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -8,4 +8,19 @@ module AdsHelper
     user_signed_in? && (current_user.owns_ad? ad)
   end
 
+  def status_label(ad)
+    colors = {
+        published:       'green',
+        draft:           'yellow',
+        waiting_review:  'yellow',
+        paused:          'yellow',
+        stopped:         'red',
+        deleted:         'red'
+    }
+    colors.default = 'black'
+  
+    color = colors[ad.status.to_sym]
+    content_tag :span, ad.decorate.display_status.titlecase, class: "status-label status-label--#{color}"
+  end
+
 end
