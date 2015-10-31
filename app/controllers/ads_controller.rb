a52fbf3de03972d107e16f89c4c61126e67c37da
Rune Halvorsen <runeh@plendit.com>
2015-10-31 20:50:55 +0100

    Fixed wrong name in prop whitelist for payin_rules

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -281,7 +281,7 @@ class AdsController < ApplicationController
       params.require(:ad).permit( :title, :body, :price_in_h, :tag_list, :insurance_required,
         :registration_number, :location_id,
         :location_attributes => [:address_line, :post_code],
-        :payin_rules_attributes => [:amount_in_h, :unit, :effective_from, :id, :_destroy])
+        :payin_rules_attributes => [:payin_amount_in_h, :unit, :effective_from, :id, :_destroy])
     end
 
 
