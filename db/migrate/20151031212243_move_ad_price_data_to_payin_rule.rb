7112af4e7325641143e8431efb95d57673ab0a5a
Rune Halvorsen <runeh@plendit.com>
2015-11-01 18:46:16 +0100

    Fixed migration

--- db/migrate/20151031212243_move_ad_price_data_to_payin_rule.rb
+++ db/migrate/20151031212243_move_ad_price_data_to_payin_rule.rb
@@ -1,7 +1,7 @@
 class MoveAdPriceDataToPayinRule < ActiveRecord::Migration
   def up
     Ad.unscoped.all.each do |a|
-      pi = a.payin_rules.find_or_initialize_by(unit: PayinRule.units[:day], effective_from: 1)
+      pi = a.payin_rules.find_or_create_by(unit: PayinRule.units[:day], effective_from: 1)
 
       pi.update_column( :payin_amount, a.price ) if pi.new_record?
       a.update_column( :price, nil )
