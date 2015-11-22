48913baaf46afb17f8072c6e0320528d842ddeb4
R. Arruda <rarruda@plendit.com>
2015-11-22 13:27:53 +0100

    use localization framework for date formats

--- config/initializers/time_formats.rb
+++
@@ -1,4 +0,0 @@
-# fixme: should be zero padded, and day should be lowercase.
-#        Maybe there's a localiztion framework for that?
-Time::DATE_FORMATS[:plendit_short_date] = '%d. %b %Y'
-Time::DATE_FORMATS[:plendit_short_datetime] = '%d.%m.%Y klokka %H.%M'
