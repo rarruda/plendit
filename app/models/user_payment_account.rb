5798bf983efdf9c088f44db1639d9d24f38893d1
Eirik <eirik@plendit.com>
2015-12-10 21:45:40 +0000

    Mangopay stuff, account number >

--- app/models/user_payment_account.rb
+++ app/models/user_payment_account.rb
@@ -60,9 +60,9 @@ class UserPaymentAccount < ActiveRecord::Base
   # only valid for norway:
   def is_bank_account_number_valid?
     # check a regex to see if its a 11 digit number
-    errors.add(:bank_account_number, "invalid account number (only numbers, 11 digits)") if self.bank_account_number !~ /\A[0-9]{11}\z/
+    errors.add(:bank_account_number, "Ugyldig kontonummer, må kun ha 11 siffer.") if self.bank_account_number !~ /\A[0-9]{11}\z/
     # remove check_digit, calculate Mod11, and see if it still matches:
-    errors.add(:bank_account_number, "invalid account number (wrong checksum)") if ( Mod11.new( self.bank_account_number[0..-2] ).full_value != self.bank_account_number )
+    errors.add(:bank_account_number, "Ugyldig kontonummer.") if ( Mod11.new( self.bank_account_number[0..-2] ).full_value != self.bank_account_number )
   end
 
   def is_bank_account_iban_valid?
