f8ce0e62b3fab638ff8d057c7abda6724ea775a3
R. Arruda <rarruda@plendit.com>
2015-12-10 01:28:34 +0100

    BookingAutoArchiveJob: improve looking, BookingAutoSetInProgressJob: use high pri queue

--- app/jobs/booking_auto_archive_job.rb
+++ app/jobs/booking_auto_archive_job.rb
@@ -2,6 +2,10 @@ class BookingAutoArchiveJob < ActiveJob::Base
   queue_as :low
 
   def perform booking
+    puts "#{DateTime.now.iso8601} Starting BookingAutoArchiveJob"
+
     booking.archive!
+
+    puts "#{DateTime.now.iso8601} Ending BookingAutoArchiveJob for #{booking.id}"
   end
 end
--- app/jobs/booking_auto_set_in_progress_job.rb
+++ app/jobs/booking_auto_set_in_progress_job.rb
@@ -1,5 +1,5 @@
 class BookingAutoSetInProgressJob < ActiveJob::Base
-  queue_as :default
+  queue_as :high
 
   def perform booking
     puts "#{DateTime.now.iso8601} Starting BookingAutoSetInProgressJob"
