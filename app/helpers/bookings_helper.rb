0444e6bf1af97f7b8509dc85984da1d1abd5663f
Rune Halvorsen <runeh@plendit.com>
2015-12-14 23:03:18 +0100

    Whitespace

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,18 +1,18 @@
 module BookingsHelper
 
-    def parse_datetime_params params, label, utc_or_local = :local
-      begin
-        year   = params[(label.to_s + '(1i)').to_sym].to_i
-        month  = params[(label.to_s + '(2i)').to_sym].to_i
-        mday   = params[(label.to_s + '(3i)').to_sym].to_i
-        hour   = (params[(label.to_s + '(4i)').to_sym] || 0).to_i
-        minute = (params[(label.to_s + '(5i)').to_sym] || 0).to_i
-        second = (params[(label.to_s + '(6i)').to_sym] || 0).to_i
+  def parse_datetime_params params, label, utc_or_local = :local
+    begin
+      year   = params[(label.to_s + '(1i)').to_sym].to_i
+      month  = params[(label.to_s + '(2i)').to_sym].to_i
+      mday   = params[(label.to_s + '(3i)').to_sym].to_i
+      hour   = (params[(label.to_s + '(4i)').to_sym] || 0).to_i
+      minute = (params[(label.to_s + '(5i)').to_sym] || 0).to_i
+      second = (params[(label.to_s + '(6i)').to_sym] || 0).to_i
 
-        return DateTime.civil_from_format(utc_or_local,year,month,mday,hour,minute,second)
-      rescue => e
-        return nil
-      end
+      return DateTime.civil_from_format(utc_or_local,year,month,mday,hour,minute,second)
+    rescue => e
+      return nil
     end
+  end
 
 end
