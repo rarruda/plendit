1f887a889c766b73bbb91f563ddd0ec6ff7dfa13
R. Arruda <rarruda@plendit.com>
2015-12-28 12:53:26 +0100

    polish/use ActiveJob for DeleteOldEmptyAdsJob

--- app/jobs/delete_old_empty_ads_job.rb
+++ app/jobs/delete_old_empty_ads_job.rb
@@ -1,11 +1,8 @@
-class DeleteOldEmptyAdsJob
-  def self.queue
-    :low
-  end
+class DeleteOldEmptyAdsJob < ActiveJob::Base
+  queue_as :low
 
-  def self.perform( age = 15.days )
+  def perform(age = 15.days)
     puts "#{DateTime.now.iso8601} Starting #{self.class.name}"
-    puts "#{DateTime.now.iso8601} Starting DeleteOldEmptyAdsJob"
 
     deleted_ads = Ad.all.unscoped.never_edited.where("created_at < ?", ( DateTime.now - age ) ).destroy_all
     if deleted_ads.length > 0
@@ -13,7 +10,6 @@ class DeleteOldEmptyAdsJob
       puts "List of ids deleted: #{deleted_ads.map(&:id).join(',')}"
     end
 
-    puts "#{DateTime.now.iso8601} Ending DeleteOldEmptyAdsJob"
     puts "#{DateTime.now.iso8601} Ending #{self.class.name}"
   end
 end
