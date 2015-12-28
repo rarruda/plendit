28744af804f6bd9ddcc22990ec1eeda77c052d13
R. Arruda <rarruda@plendit.com>
2015-12-28 16:57:14 +0100

    booking model: maximum is set by mangopay, enforce it too in the validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -74,8 +74,8 @@ class Booking < ActiveRecord::Base
   validates :from_user_id,         presence: true
   validates :user_payment_card_id, presence: true
   validates :amount,               numericality: { greater_than_or_equals: 99_00, message: 'Må være minst 99kr' }
-  validates :amount,               numericality: { less_than:         150_000_00, message: 'Kan ikke være mer enn 150.000 kroner' }
-  #validates :amount, numericality: { less_than:  25_000_00, message: 'must be at less then  25.000 kr' }, if: 'false' # KYC not provisioned
+  validates :amount,               numericality: { less_than:          25_000_00, message: 'Kan ikke være mer enn 25.000 kroner' }
+  # https://docs.mangopay.com/api-references/payins/ actual limit is 2500 eur in general...
 
   validate :validate_user_payment_card_belongs_to_from_user
 
