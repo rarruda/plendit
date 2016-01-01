8d8dabe4dc4bcd7fd864566ed30da9cc292e6cb2
Rune Halvorsen <runeh@plendit.com>
2016-01-01 22:16:36 +0100

    Added yaml file for employee info

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,6 +1,11 @@
 class MiscController < ApplicationController
   layout "article", only: [ :about, :contact, :help, :privacy, :terms, :issues ]
 
+  @@employees = YAML
+      .load_file("#{Rails.root}/config/data/employees.yml")
+      .map { |e| OpenStruct.new(e) }
+
+
   def frontpage
     @hide_search_field = true
     @ads = Ad.published
@@ -8,6 +13,10 @@ class MiscController < ApplicationController
              .limit(6)
              .map { |ad| RecursiveOpenStruct.new(ad.as_indexed_json) }
     @hero_video = Rails.configuration.x.frontpage.hero_videos.sample
+  end
+
+  def about
+    @employees = @@employees
   end
 
   def postal_place
