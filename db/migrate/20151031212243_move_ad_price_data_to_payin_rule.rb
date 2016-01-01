8f46ffafb5e2a5a47531b2d36c97cc4a4504242e
R. Arruda <rarruda@plendit.com>
2016-01-01 16:47:00 +0100

    bugfix on an old migration. field name is actually price and not amount in ad table (back when we had prices in the ad table).

--- db/migrate/20151031212243_move_ad_price_data_to_payin_rule.rb
+++ db/migrate/20151031212243_move_ad_price_data_to_payin_rule.rb
@@ -1,7 +1,7 @@
 class MoveAdPriceDataToPayinRule < ActiveRecord::Migration
   def up
     execute "DELETE FROM payin_rules;"
-    execute "INSERT INTO payin_rules ( ad_id, payin_amount, effective_from, unit ) SELECT a.id, a.amount, 1, 2 FROM ads AS a;"
+    execute "INSERT INTO payin_rules ( ad_id, payin_amount, effective_from, unit ) SELECT a.id, a.price, 1, 2 FROM ads AS a;"
   end
 
   def down
