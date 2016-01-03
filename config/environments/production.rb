40ad01f43f34a0d2bb62a9089be6372a9b201117
R. Arruda <rarruda@plendit.com>
2016-01-04 00:40:39 +0100

    tune production config: - always enable ssl - never serve static files - if ever hits static cache, then cache it for 6 hours

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = false
 
-  # to enable some caching for varnish/clientsd:
-  #config.static_cache_control = "public, max-age=#{1.day.to_i}"
+  # to enable some caching for cdn/clients:
+  config.static_cache_control = "public, max-age=#{6.hours.to_i}"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(
@@ -48,7 +48,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
