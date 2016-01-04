facfe00ba9b7b8f1b38a4037feebcfe29eee9595
R. Arruda <rarruda@plendit.com>
2016-01-05 00:18:27 +0100

    move s3 images to new url scheme

--- app/models/ad_image.rb
+++ app/models/ad_image.rb
@@ -19,8 +19,7 @@ class AdImage < ActiveRecord::Base
     },
     :default_url => "/images/:style/missing.png",
     # FIXME: Fix image url, so it wont get blocked by ad-blockers:
-    #:path        => "/images/listings/:ad_id/:style/:hash.:extension",
-    :path        => "/images/ads/:ad_id/:style/:hash.:extension",
+    :path        => "/images/listings/:ad_id/:style/:hash.:extension",
     :hash_data   => ":class/:attachment/:id",
     :hash_secret => ENV['PCONF_PAPERCLIP_HASH_SALT'],
   }
