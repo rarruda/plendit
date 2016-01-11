a980450b6d715549033f5bd200bc518b57f96795
Rune Halvorsen <runeh@plendit.com>
2016-01-11 22:02:46 +0100

    Added icon helper for card icons

--- app/helpers/user_payment_cards_helper.rb
+++ app/helpers/user_payment_cards_helper.rb
@@ -1,2 +1,14 @@
 module UserPaymentCardsHelper
+
+    def payment_card_icon card_type
+        if card_type == 'VISA'
+          icon 'cc-visa', class: 'fa-lg'
+        elsif card_type == 'MASTERCARD'
+          icon 'cc-mastercard', class: 'fa-lg'
+        else
+          icon 'credit-card', class: 'fa-lg'
+        end
+    end
+
+
 end
