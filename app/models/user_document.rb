4d8c5e4e98d12e4958250c5976f502a1013a0c7b
R. Arruda <rarruda@plendit.com>
2016-01-13 00:39:25 +0100

    lint

--- app/models/user_document.rb
+++ app/models/user_document.rb
@@ -6,18 +6,18 @@ class UserDocument < ActiveRecord::Base
   has_attached_file :document, {
     #:styles        => { thumbnail: "200x200>" }
     # https://github.com/thoughtbot/paperclip#uri-obfuscation
-    :path           => "/documents/users/:user_id/:hash.:extension", # :filename
-    :preserve_files => true, #true for Soft-delete (delete only from database, not from storage/s3)
-    :hash_data      => ":class/:attachment/:id",
-    :hash_secret    => ENV['PCONF_DOCUMENTS_PAPERCLIP_HASH_SALT'],
-    :default_url    => nil,
-    :url            => ":s3_domain_url",
-    :s3_host_alias  => nil,
-    :s3_permissions => :private,
-    :s3_credentials => {
-      :bucket            => ENV['PCONF_DOCUMENTS_S3_BUCKET_NAME'],
-      :access_key_id     => ENV['PCONF_DOCUMENTS_AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['PCONF_DOCUMENTS_AWS_SECRET_ACCESS_KEY']
+    path:            "/documents/users/:user_id/:hash.:extension", # :filename
+    preserve_files:  true, #true for Soft-delete (delete only from database, not from storage/s3)
+    hash_data:       ":class/:attachment/:id",
+    hash_secret:     ENV['PCONF_DOCUMENTS_PAPERCLIP_HASH_SALT'],
+    default_url:     nil,
+    url:             ":s3_domain_url",
+    s3_host_alias:   nil,
+    s3_permissions:  :private,
+    s3_credentials:  {
+      bucket:            ENV['PCONF_DOCUMENTS_S3_BUCKET_NAME'],
+      access_key_id:     ENV['PCONF_DOCUMENTS_AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['PCONF_DOCUMENTS_AWS_SECRET_ACCESS_KEY']
     },
   }
 
@@ -25,9 +25,9 @@ class UserDocument < ActiveRecord::Base
   enum status:   { not_approved: 1, pending_approval: 2, approved: 3 }
   # status: pending_approval 2=>1(pending_approval=>waiting_approval), approved: 3=>5, not_approved: 1=>10
 
-  validates_attachment_content_type :document, :content_type => ["application/pdf", "image/jpeg", "image/jpg", "image/png"]
-  validates_attachment_file_name    :document, :matches => [/png\Z/i, /jpe?g\Z/i, /pdf\Z/i]
-  validates_attachment_size         :document, :in => 0..7.megabytes
+  validates_attachment_content_type :document, content_type: ["application/pdf", "image/jpeg", "image/jpg", "image/png"]
+  validates_attachment_file_name    :document, matches: [/png\Z/i, /jpe?g\Z/i, /pdf\Z/i]
+  validates_attachment_size         :document, in: 0..7.megabytes
 
   validates_uniqueness_of :guid
 
