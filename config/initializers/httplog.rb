c2e00998180b4823e5dd2c023ab0fff2805b6db4
R. Arruda <rarruda@plendit.com>
2016-01-20 02:20:21 +0100

    stop tracing traffic to airbrake and localhost

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -1,5 +1,5 @@
 
 # HttpLog is not enabled in production.
 if defined? HttpLog
-  HttpLog.options[:url_blacklist_pattern] = "(.*)\.newrelic\.com(\:[0-9]*|)"
+  HttpLog.options[:url_blacklist_pattern] = "http(s?)\://(.*)(newrelic\.com|airbrake\.io|localhost)(\:[0-9]*|)"
 end
