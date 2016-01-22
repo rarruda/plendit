ea75f954b1fe40554d9bd0e51d8d2a12e6e985ab
R. Arruda <rarruda@plendit.com>
2016-01-22 03:01:48 +0100

    initial support for mangopay webhook support

--- app/admin/mangopay_webhook.rb
+++ app/admin/mangopay_webhook.rb
@@ -8,10 +8,10 @@ ActiveAdmin.register MangopayWebhook do
   index do
     selectable_column
     id_column
-    column :event_type_mp
+    column :event_type
     column :resource_type
     column :resource_vid
-    column :date_mp
+    column :timestamp_v
     column(:status)   {|ad| status_tag(ad.status) }
     column :remote_ip
     column :raw_data
