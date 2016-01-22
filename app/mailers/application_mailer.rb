267e3bbfce08bc08fac014ed269e0fa9464ff11a
R. Arruda <rarruda@plendit.com>
2016-01-22 18:12:09 +0100

    update insurance documents, and mailer to send them

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -182,9 +182,11 @@ class ApplicationMailer < ActionMailer::Base
     attach_files << "Forsikringsvilkaar_Korttidsforsikring_Ting.pdf"  if @booking.ad.bap?
     attach_files << "Forsikringsbevis_Innbo.pdf"                      if @booking.ad.realestate?
     attach_files << "Forsikringsvilkaar_Korttidsforsikring_Innbo.pdf" if @booking.ad.realestate?
-    attach_files << "Forsikringsvilkaar_Korttidsforsikring_motorvogn.pdf" if @booking.ad.motor?
+    attach_files << "Forsikringsbevis_Baat.pdf"                       if @booking.ad.boat?
     attach_files << "Forsikringsvilkaar_Korttidsforsikring_Baat.pdf"  if @booking.ad.boat?
     attach_files << "Sammendrag_av_Korttidsforsikring_Baat.pdf"       if @booking.ad.boat?
+    attach_files << "Forsikringsbevis_motorvogn.pdf"                      if @booking.ad.motor?
+    attach_files << "Forsikringsvilkaar_Korttidsforsikring_motorvogn.pdf" if @booking.ad.motor?
 
     if @booking.ad.motor?
       attach_files << "Sammendrag_av_Korttidsforsikring_motorvogn_Bil.pdf"     if @booking.ad.car?
