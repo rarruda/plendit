b5cd920d2e2f323ea472d3aec826663404385ab6
R. Arruda <rarruda@plendit.com>
2016-01-27 18:03:35 +0100

    add warning about enabling assets via cdn (it only mostly works).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
   #config.action_controller.asset_host = 'https://cdn.plendit.com'
+  # ^^ breaks image assets... cant really reconfigure either as same path is used in S3 for image buckets.
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
