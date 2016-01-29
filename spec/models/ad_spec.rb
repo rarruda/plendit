41775dfb21f3fce593ab65df63fd3abf16f21859
R. Arruda <rarruda@plendit.com>
2016-01-29 01:18:37 +0100

    small refactoring of testing/spec of the Ad model

--- spec/models/ad_spec.rb
+++ spec/models/ad_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 
-RSpec.describe Ad, :type => :model do
+RSpec.describe Ad, type: :model do
+  #it_behaves_like "Searcheable"
+
+  let(:ad_bap) {
+    FactoryGirl.build_stubbed(:ad_bap,
+      body: "foo bar 123",
+      user: FactoryGirl.build_stubbed(:user),
+      ad_images: [ FactoryGirl.build_stubbed(:ad_image) ],
+      payin_rules: [ FactoryGirl.build_stubbed(:payin_rule, ad: FactoryGirl.build_stubbed(:ad_bap)) ]
+    )
+  }
 
   let(:ad_bap_not_stubbed) {
     ad = FactoryGirl.build(:ad_bap,
@@ -12,39 +22,37 @@ RSpec.describe Ad, :type => :model do
     )
   }
 
-  it "should work on a valid model" do
-    ad = FactoryGirl.build_stubbed(:ad_bap,
-      body: "foo bar 123",
-      user: FactoryGirl.build_stubbed(:user),
-      ad_images: [ FactoryGirl.build_stubbed(:ad_image) ],
-      payin_rules: [ FactoryGirl.build_stubbed(:payin_rule, ad: FactoryGirl.build_stubbed(:ad_bap)) ]
-    )
-    expect(ad).to be_valid
-  end
-
-  it "should work on a model for category motor with a valid registration number" do
-    ad = FactoryGirl.build_stubbed(:ad_motor,
+  let(:ad_motor) {
+    FactoryGirl.build_stubbed(:ad_motor,
       body: "foo bar 123",
       user: FactoryGirl.build_stubbed(:user),
       ad_images: [ FactoryGirl.build_stubbed(:ad_image) ],
       payin_rules: [ FactoryGirl.build_stubbed(:payin_rule, ad: FactoryGirl.build_stubbed(:ad_motor) )  ]
     )
-    expect(ad).to be_valid
+  }
+
+  it "should work on a valid model" do
+    expect(ad_bap).to be_valid
+  end
+
+  it "should work on a model for category motor with a valid registration number" do
+    ad_motor.registration_number = 'NN123456'
+
+    expect(ad_motor).to be_valid
   end
 
   it "should fail on a model for category motor without registration number" do
-    ad = FactoryGirl.build_stubbed(:ad_motor, user: FactoryGirl.build_stubbed(:user),
-      registration_number: nil
-    )
-    expect(ad).not_to be_valid
+    ad_motor.registration_number = nil
+
+    expect(ad_motor).not_to be_valid
   end
 
-  it "should fail on a model for category motor with an invalid registration number" do
-    ad = FactoryGirl.build_stubbed(:ad_motor, user: FactoryGirl.build_stubbed(:user),
-      registration_number: 'FOOBAR123X'
-    )
-    expect(ad).not_to be_valid
-  end
+  # we do not validate currently registration number:
+  # xit "should fail on a model for category motor with an invalid registration number" do
+  #   ad_motor.registration_number = 'FOOBAR123X'
+
+  #   expect(ad_motor).not_to be_valid
+  # end
 
   it "should set state to edited if the payin_rules has changed" do
     # needs to be valid for the rest of the test to work:
