2fbf04f8d89434eeab23d067ed3f466f3495c8e1
Rune Halvorsen <runefh@gmail.com>
2016-01-31 13:05:48 +0100

    Cleaned up unavailability method

--- app/models/ad_item.rb
+++ app/models/ad_item.rb
@@ -3,10 +3,14 @@ class AdItem < ActiveRecord::Base
   has_many :bookings #, dependent: :nullify
 
   def unavailability
-    bookings = Booking.reserved.ad_item( self.id ).order( :starts_at )
-    bookings = bookings.map { |b| (b.starts_at.to_date..b.ends_at.to_date).to_a }.flatten
-    unavailabilities = self.ad.unavailabilities.map { |b| (b.starts_at..b.ends_at).to_a }.flatten
-    bookings + unavailabilities
+    bookings = Booking.reserved.ad_item( self.id )
+    unavailabilities = self.ad.unavailabilities
+
+    items = bookings + unavailabilities
+    items
+        .map { |b| (b.starts_at.to_date..b.ends_at.to_date).to_a }
+        .flatten
+        .sort
   end
 
 end
