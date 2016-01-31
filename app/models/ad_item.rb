ccc07534dc1f9ad16f31ffa2b156c153afad2114
Rune Halvorsen <runefh@gmail.com>
2016-01-31 12:43:57 +0100

    Updated unavailability method to take into account unavailabilities model

--- app/models/ad_item.rb
+++ app/models/ad_item.rb
@@ -4,7 +4,9 @@ class AdItem < ActiveRecord::Base
 
   def unavailability
     bookings = Booking.reserved.ad_item( self.id ).order( :starts_at )
-    bookings.map { |b| (b.starts_at.to_date..b.ends_at.to_date).to_a }.flatten
+    bookings = bookings.map { |b| (b.starts_at.to_date..b.ends_at.to_date).to_a }.flatten
+    unavailabilities = self.ad.unavailabilities.map { |b| (b.from_date..b.to_date).to_a }.flatten
+    bookings + unavailabilities
   end
 
 end
