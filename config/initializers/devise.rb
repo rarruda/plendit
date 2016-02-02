af0d1db35148d4aa55b30ef5a863f2aad6350e36
Rune Halvorsen <runefh@gmail.com>
2016-02-02 01:27:35 +0100

    Added params to fetch firstname and lastname from fb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -235,8 +235,12 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['PCONF_FACEBOOK_APP_ID'], ENV['PCONF_FACEBOOK_SECRET'],
-    scope: 'email,user_birthday'
+  config.omniauth :facebook,
+        ENV['PCONF_FACEBOOK_APP_ID'],
+        ENV['PCONF_FACEBOOK_SECRET'],
+        scope: 'email,user_birthday',
+        info_fields: 'email,name,first_name,last_name,birthday'
+
   config.omniauth :google_oauth2, ENV['PCONF_GOOGLE_CLIENT_ID'], ENV['PCONF_GOOGLE_CLIENT_SECRET'],
     { name: 'google', image_aspect_ratio: 'square', skip_jwt: true } #, :image_size=> 120 }
   # see https://github.com/zquestz/omniauth-google-oauth2 for list of parameters
