aef783f66a9b26e22a9dba9250ca332040a2d6e5
Rune Halvorsen <runeh@plendit.com>
2016-04-25 00:05:20 +0200

    Save current user on request if there is one

--- app/controllers/wanted_item_request_controller.rb
+++ app/controllers/wanted_item_request_controller.rb
@@ -6,6 +6,9 @@ class WantedItemRequestController < ApplicationController
 
   def create
     @wanted_item_request = WantedItemRequest.new(wanted_item_request_params)
+    if user_signed_in?
+      @wanted_item_request.user = current_user
+    end
     @wanted_item_request.save
     redirect_to users_wanted_item_request_index_path
   end
